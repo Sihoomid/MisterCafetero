c5553795067547a71709f18febf9c26020cb21fb
Eduardo Cremades <sihoomid@gmail.com>
2023-07-13 14:30:28 +0200

    Primeros resultados finales.

--- db_handler.py
+++ db_handler.py
@@ -17,6 +17,7 @@ class Handler:
                     " days text,"
                     " start_time time,"
                     " active bool,"
+                    " launched bool,"
                     " PRIMARY KEY (id))")
         # Tabla log.
         con.execute("CREATE TABLE log"
@@ -27,15 +28,42 @@ class Handler:
                     " PRIMARY KEY (id))")
         con.close()
 
+    def insert_new_log(self, task_id: int, message: str, status: str) -> None:
+        con = sqlite3.connect(self.database_path)
+        cursor = con.execute("SELECT MAX(id) FROM log")
+        row = cursor.fetchone()
+        newindex = 1
+        if row != None:
+            if row[0] != None:
+                newindex = int(str(row[0])) + 1
+        cursor.close()
+        con.execute(
+            "INSERT INTO log (id, task_id, message, status) VALUES ({}, {}, '{}', '{}')".format(
+            newindex,
+            task_id,
+            message.replace("'", "\""),
+            status))
+        con.commit()
+        con.close()
+
+    def mark_launched_task(self, task_id: int) -> None:
+        con = sqlite3.connect(self.database_path)
+        con.execute(
+            "UPDATE task SET launched = 1 WHERE id = {}".format(task_id))
+        con.commit()
+        con.close()
+
     def pending_tasks(self) -> "list[task.Task]":
         con = sqlite3.connect(self.database_path)
-        cursor = con.execute("SELECT *"
-                             " FROM task"
-                             " WHERE days LIKE '%{}%'"
-                             " AND start_time <= '{}'"
-                             " AND active = 1".format(datetime.datetime.today().weekday(),
-                                                      datetime.datetime.today().time()
-                                                      .strftime("%H:%M:%S")))
+        cursor = con.execute(
+            "SELECT *"
+            " FROM task"
+            " WHERE days LIKE '%{}%'"
+            " AND start_time <= '{}'"
+            " AND active = 1"
+            " AND launched = 0".format(datetime.datetime.today().weekday(),
+                                    datetime.datetime.today().time()
+                                    .strftime("%H:%M:%S")))
         return_list = []
         for row in cursor.fetchall():
             t = task.Task()
@@ -46,5 +74,23 @@ class Handler:
             t.start_time = row[4]
             t.active = row[5]
             return_list.append(t)
+        cursor.close()
+        con.close()
 
         return return_list
+    
+    def unmark_tasks(self) -> None:
+        con = sqlite3.connect(self.database_path)
+        con.execute(
+            "UPDATE task SET launched = 0 WHERE days NOT LIKE '%{}%'".format(
+            datetime.datetime.today().weekday()
+            )
+        )
+        con.execute(
+            "UPDATE task SET launched = 0 WHERE days LIKE '%{}%' AND start_time > '{}'".format(
+            datetime.datetime.today().weekday(),
+            datetime.datetime.today().time()
+            )
+        )
+        con.commit()
+        con.close()        
--- mister_cafetero.py
+++ mister_cafetero.py
@@ -1,5 +1,37 @@
-print("Mister Cafetero\n----\nEjecutador de tareas programadas\n--------")
+import datetime
+from os import path
+from subprocess import check_output
+from time import sleep
+
+import db_handler
+
+print("Mister Cafetero\n----\nEjecutor de tareas programadas\n--------")
 print("\nPreparando café...\n")
 
+dbHandler = db_handler.Handler("tasks.sqlite3")
+if not path.exists("tasks.sqlite3"):
+    dbHandler.create_database()
+
 while True:
-    pass
+    sleep(5)
+    dbHandler.unmark_tasks()
+    pendTasks = dbHandler.pending_tasks()
+    
+    for t in pendTasks:
+        print("¡Un café! --> El {} a las {} lanza \"{}\""
+              .format(datetime.datetime.today().strftime("%d/%m/%y"),
+                      datetime.datetime.today().strftime("%H:%M:%S"),
+                      t.name))
+        # Lanza la tarea.
+        okResult = True
+        msgResult = "Bien tirado"
+        statusResult = "OK"
+        try:
+            check_output(t.command, shell=False)
+        except:
+            okResult = False
+            msgResult = "Error lanzando la tarea"
+            statusResult = "ERROR"
+        dbHandler.insert_new_log(t.id, msgResult, statusResult)
+        # Deja la tarea como ya lanzada en el programador.
+        dbHandler.mark_launched_task(t.id)
